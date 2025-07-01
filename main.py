b41f52930fd3884974a1121dfe6cd10e189a0e6f
thanh123kte <aye123kte@gmail.com>
2025-07-01 14:32:29 +0700

    update todo and mount link for static

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 
 from routers import user, address, restaurant, category, menu_item 
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(user.router)
 app.include_router(address.router)
